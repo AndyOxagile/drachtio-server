21b2ae90ef45e7ce8dd2359c266c3bd6585f872e
Andrey Savitsky <andrey.savitsky@oxagile.com>
2025-10-31 14:20:05 +0300

    23

--- src/controller.cpp
+++ src/controller.cpp
@@ -1461,9 +1461,10 @@ namespace drachtio {
                 auto error_header = sip->sip_error;
                 auto er_arr = error_header->er_common;
                 DR_LOG(log_error) << "DrachtioController::processMessageStatelessly: modify start";
-                DR_LOG(log_error) << "DrachtioController::processMessageStatelessly: modify start: " << (sizeof(er_arr)/sizeof(*er_arr));
+                DR_LOG(log_error) << "DrachtioController::processMessageStatelessly: modify sizeof(er_arr): " << sizeof(er_arr) ;
+                DR_LOG(log_error) << "DrachtioController::processMessageStatelessly: modify sizeof(*er_arr): " << sizeof(*er_arr);
                 DR_LOG(log_error) << "DrachtioController::processMessageStatelessly: modify start: " << (char *)error_header->er_common[0].h_data;
-                                
+
                 if (error_header->er_common[0].h_data && error_header->er_common[0].h_len > 0) {
                     std::string error_string((const char *) error_header->er_common[0].h_data, error_header->er_common[0].h_len);
                     DR_LOG(log_error) << "DrachtioController::processMessageStatelessly: discarding message due to error: " << error_string;
